6fd83831eda462c6e312bb6d911fbe93ccfaefe6
Denian-619 <denianbatista5@gmail.com>
2022-01-24 23:53:51 -0300

    criado o atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=None):
+    def __init__(self, *filhos, nome=None, idade=None):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Denian', 21)
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    print(p.idade)
+    maite = Pessoa(nome='Maite', idade=0)
+    denian = Pessoa(maite, noa, nome='Denian', idade=21)
+    print(Pessoa.cumprimentar(denian))
+    print(id(denian))
+    print(denian.cumprimentar())
+    print(denian.nome)
+    print(denian.idade)
+    for filho in denian.filhos:
+        print(filho.nome)
