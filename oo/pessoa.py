221af6fa78c7b3a33dae12fb9e266982f58236cb
Denian-619 <denianbatista5@gmail.com>
2022-01-25 00:22:42 -0300

    criado atributo de classe olhos para a classe Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos = 2
     def __init__(self, *filhos, nome=None, idade=None):
         self.idade = idade
         self.nome = nome
@@ -22,3 +23,26 @@ if __name__ == '__main__':
     print(denian.__dict__)
     del denian.sobrenome
     print(denian.__dict__)
+    print(maite.__dict__)
+    print(Pessoa.olhos)
+    print(denian.olhos)
+    print(maite.olhos)
+    denian.olhos = 1
+    print()
+    print(denian.olhos)
+    print(denian.__dict__)
+    print(Pessoa.olhos)
+    print(maite.olhos)
+    Pessoa.olhos = 3
+    print()
+    print(Pessoa.olhos)
+    print(denian.__dict__)
+    print(denian.olhos)
+    print(maite.olhos)
+    print()
+    del denian.olhos
+    print(Pessoa.olhos)
+    print(denian.__dict__)
+    print(denian.olhos)
+    print(maite.olhos)
+
